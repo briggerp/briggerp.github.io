11c5e208685d500b7533dfc7bf064d6d9280fa7e
briggerp <briggerp@student.ethz.ch>
2024-06-22 15:23:30 +0200

    undo

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,82 +125,76 @@
 
 	// Nav Panel.
 
-	// Toggle.
-	$navPanelToggle = $(
-		'<a href="#navPanel" id="navPanelToggle">Menu</a>'
-	).appendTo($wrapper);
+		// Toggle.
+			$navPanelToggle = $(
+				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+			)
+				.appendTo($wrapper);
 
-	// Change toggle styling once we've scrolled past the header.
-	$header.scrollex({
-		bottom: '5vh',
-		enter: function() {
-			$navPanelToggle.removeClass('alt');
-		},
-		leave: function() {
-			$navPanelToggle.addClass('alt');
-		}
-	});
+			// Change toggle styling once we've scrolled past the header.
+				$header.scrollex({
+					bottom: '5vh',
+					enter: function() {
+						$navPanelToggle.removeClass('alt');
+					},
+					leave: function() {
+						$navPanelToggle.addClass('alt');
+					}
+				});
 
-	// Panel.
-	$navPanel = $(
-		'<div id="navPanel">' +
-			'<nav></nav>' +
-			'<a href="#navPanel" class="close"></a>' +
-		'</div>'
-	).appendTo($body)
-	.panel({
-		delay: 500,
-		hideOnClick: true,
-		hideOnSwipe: true,
-		resetScroll: true,
-		resetForms: true,
-		side: 'right',
-		target: $body,
-		visibleClass: 'is-navPanel-visible'
-	});
+		// Panel.
+			$navPanel = $(
+				'<div id="navPanel">' +
+					'<nav>' +
+					'</nav>' +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-navPanel-visible'
+				});
 
-	// Get inner.
-	$navPanelInner = $navPanel.children('nav');
+			// Get inner.
+				$navPanelInner = $navPanel.children('nav');
 
-	// Move nav content on breakpoint change.
-	var $navContent = $nav.children();
+			// Move nav content on breakpoint change.
+				var $navContent = $nav.children();
 
-	breakpoints.on('>medium', function() {
-		// NavPanel -> Nav.
-		$navContent.appendTo($nav);
+				breakpoints.on('>medium', function() {
 
-		// Flip icon classes.
-		$nav.find('.icons, .icon').removeClass('alt');
+					// NavPanel -> Nav.
+						$navContent.appendTo($nav);
 
-		// Ensure the nav is fixed at the top on larger screens.
-		$nav.css({
-			'position': 'fixed',
-			'top': 0,
-			'left': 0,
-			'width': '100%',
-			'z-index': 2
-		});
-	});
+					// Flip icon classes.
+						$nav.find('.icons, .icon')
+							.removeClass('alt');
 
-	breakpoints.on('<=medium', function() {
-		// Nav -> NavPanel.
-		$navContent.appendTo($navPanelInner);
+				});
 
-		// Flip icon classes.
-		$navPanelInner.find('.icons, .icon').addClass('alt');
+				breakpoints.on('<=medium', function() {
 
-		// Reset nav position for smaller screens.
-		$nav.css({
-			'position': 'relative',
-			'width': 'auto'
-		});
-	});
+					// Nav -> NavPanel.
+						$navContent.appendTo($navPanelInner);
 
-	// Hack: Disable transitions on WP.
-	if (browser.os == 'wp' && browser.osVersion < 10) {
-		$navPanel.css('transition', 'none');
-	}
+					// Flip icon classes.
+						$navPanelInner.find('.icons, .icon')
+							.addClass('alt');
 
+				});
+
+			// Hack: Disable transitions on WP.
+				if (browser.os == 'wp'
+				&&	browser.osVersion < 10)
+					$navPanel
+						.css('transition', 'none');
 
 	// Intro.
 		var $intro = $('#intro');
